f771e782193177550b251f40029fe2cd2718cc9e
herpeb <pebbiandra@gmail.com>
2024-03-13 15:11:21 +0700

    Latihan variabel pada python
    pengumpulan latihan variabel pada python

---
+++ latihan_variabel.py
@@ -0,0 +1,7 @@
+nama = input("Masukan nama anda : ")
+alamat = input("Masukan alamat anda : ")
+tlp = input("Masukan nomor wa/telp : ")
+kode_pos = input("Masukan kodepos : ")
+
+print("{:<10}{:<20}{:<10}{:<10}".format("Nama","Alamat","Tlp","Kode Pos"))
+print("{:<10}{:<20}{:<10}{:<10}".format(nama,alamat,tlp,kode_pos))
